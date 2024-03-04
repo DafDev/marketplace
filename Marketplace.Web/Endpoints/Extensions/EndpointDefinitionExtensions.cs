95a68db8a75b78dc1590dedda79317b0996da09b
Afahd Ali <afahd.ali@epc-groupe.com>
2024-03-04 14:21:49 +0100

    added application layer and defined api endpoints

---
+++ Marketplace.Web/Endpoints/Extensions/EndpointDefinitionExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Marketplace.Web.Endpoints.Extensions;
+
+public static class EndpointDefinitionExtensions
+{
+    public static IServiceCollection AddEndpointDefinitions(this IServiceCollection services, params Type[] scanMarkers)
+    {
+        var endpointDefinitions = new List<IEndpointDefinition>();
+
+        foreach (var scanMarker in scanMarkers)
+        {
+            endpointDefinitions.AddRange(
+                scanMarker.Assembly.ExportedTypes
+                    .Where(type => typeof(IEndpointDefinition).IsAssignableFrom(type) && !type.IsInterface)
+                    .Select(Activator.CreateInstance).Cast<IEndpointDefinition>()
+            );
+        }
+
+        services.AddSingleton(endpointDefinitions as IReadOnlyCollection<IEndpointDefinition>);
+        return services;
+    }
+
+    public static void UseEndpointDefinitions(this WebApplication app)
+    {
+        var endpointDefinitions = app.Services.GetRequiredService<IReadOnlyCollection<IEndpointDefinition>>();
+
+        foreach (var endpointDefinition in endpointDefinitions)
+        {
+            endpointDefinition.DefineEndpoints(app);
+        }
+    }
+}
---
+++ Marketplace.Web/Endpoints/MarketplaceEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Marketplace.Web.Endpoints;
+
+public class MarketplaceEndpoints : IEndpointDefinition
+{
+    public void DefineEndpoints(WebApplication app)
+    {
+        app.MapPost("/ad", CreateAd);
+    }
+
+    public async Task<IResult> CreateAd()
+    {
+        return Results.Created();
+    }
+
+}
