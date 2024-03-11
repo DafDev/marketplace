d76e766f71dee783b4203bf0f53277d0567a2d23
Daf <afahd.ali@gmail.com>
2024-03-11 12:18:16 +0100

    created repository interface in domain

---
+++ Marketplace.Domain/Contexts/Ad/Repositories/IClassifiedAdRepository.cs
@@ -0,0 +1,13 @@
+﻿using Marketplace.Domain.Contexts.Ad.Entities;
+using Marketplace.Domain.Contexts.Ad.ValueObjects;
+
+namespace Marketplace.Domain.Contexts.Ad.Repositories;
+
+public interface IClassifiedAdRepository
+{
+    Task<bool> Exists(ClassifiedAdId id);
+
+    Task<ClassifiedAd> Load<ClassifiedAd>(ClassifiedAdId id);
+
+    Task Add(ClassifiedAd entity);
+}
