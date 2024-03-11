50ddac627ac5398d4e343ff775337b6dede3b79d
Daf <afahd.ali@gmail.com>
2024-03-11 15:29:30 +0100

    handled api endopoints in a more concised manner

--- Marketplace.Web/Endpoints/MarketplaceEndpoints.cs
+++ Marketplace.Web/Endpoints/MarketplaceEndpoints.cs
@@ -1,11 +1,13 @@
 ﻿using Marketplace.Application.Contracts.V1;
 using Marketplace.Application.Services.Ad;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Marketplace.Web.Endpoints;
 
 public class MarketplaceEndpoints : IEndpointDefinition
 {
-      public void DefineEndpoints(WebApplication app)
+    private static readonly ILogger Log = (ILogger)Serilog.Log.ForContext<MarketplaceEndpoints>();
+    public void DefineEndpoints(WebApplication app)
     {
         app.MapPost("/ad", CreateAd);
         app.MapPut("/ad/title", SetTitle);
@@ -15,32 +17,32 @@ public class MarketplaceEndpoints : IEndpointDefinition
     }
 
     public async Task<IResult> CreateAd(Create command, IApplicationService classifiedAdsApplicationServices)
-    {
-        await classifiedAdsApplicationServices.Handle(command);
-        return Results.Created();
-    }
+        => await HandleRequest(command, classifiedAdsApplicationServices.Handle);
 
     public async Task<IResult> SetTitle(SetTitle command, IApplicationService classifiedAdsApplicationServices)
-    {
-        await classifiedAdsApplicationServices.Handle(command);
-        return Results.Ok();
-    }
+        => await HandleRequest(command, classifiedAdsApplicationServices.Handle);
 
     public async Task<IResult> UpdateText(UpdateText command, IApplicationService classifiedAdsApplicationServices)
-    {
-        await classifiedAdsApplicationServices.Handle(command);
-        return Results.Ok();
-    }
-    
+        => await HandleRequest(command, classifiedAdsApplicationServices.Handle);
+
     public async Task<IResult> UpdatePrice(UpdatePrice command, IApplicationService classifiedAdsApplicationServices)
-    {
-        await classifiedAdsApplicationServices.Handle(command);
-        return Results.Ok();
-    }
+        => await HandleRequest(command, classifiedAdsApplicationServices.Handle);
 
     public async Task<IResult> RequestToPublish(RequestToPublish command, IApplicationService classifiedAdsApplicationServices)
+        => await HandleRequest(command, classifiedAdsApplicationServices.Handle);
+
+    private static async Task<IResult> HandleRequest<T>(T request, Func<T, Task> handler)
     {
-        await classifiedAdsApplicationServices.Handle(command);
-        return Results.Ok();
+        try
+        {
+            Log.LogDebug("Handling HTTP request of type {type}", typeof(T).Name);
+            await handler(request);
+            return Results.Ok();
+        }
+        catch (Exception e)
+        {
+            Log.LogDebug("Error handling the request, {errorMessage}", e.Message);
+            return Results.BadRequest(new { error = e.Message, stackTrace = e.StackTrace });
+        }
     }
 }
